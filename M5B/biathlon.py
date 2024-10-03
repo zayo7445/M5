6c93b7e04cc797ed3cb1af46f88c5fbba886b7db
Zaidoun Khalid <khalidzaidoun@icloud.com>
2024-10-03 17:28:19 +0200

    Added error handling

--- M5B/biathlon.py
+++ M5B/biathlon.py
@@ -2,30 +2,45 @@
 
 import random
 
+
 def init_game():
     print("~" * 38)
     print("              Biathlon")
     print("         a hit or miss game")
     print("~" * 38)
-    print("You got 5 shots\n")
+    print("You got 5 shots")
+
 
 def init_targets():
     return ["*"] * 5
 
+
 def show_targets(targets: list):
-    print("1 2 3 4 5")
+    print("\n1 2 3 4 5")
     print(*targets)
 
+
 def shoot(targets: list, pos: int):
+    if targets[pos] == "O":
+        print("Hit on closed target")
+        return
     hit = random.choice([True, False])
     if hit:
-        if targets[pos] == "*":
-            print("Hit on open target")
-            targets[pos] = "O"
-        elif targets[pos] == "O":
-            print("Hit on closed target")
+        print("Hit on open target")
+        targets[pos] = "O"
     else:
         print("Miss")
+
+
+def get_input(i: int):
+    while True:
+        pos = input(f"Shot nr {i} at: ")
+        if pos.isdigit():
+            pos = int(pos)
+            if 1 <= pos <= 5:
+                return pos - 1
+        print("Invalid input. Try again.\n")
+
 
 def main():
     init_game()
@@ -33,9 +48,10 @@ def main():
 
     for i in range(1, 6):
         show_targets(targets)
-        pos = int(input(f"Shot nr {i} at: ")) - 1
+        pos = get_input(i)
         shoot(targets, pos)
 
+    show_targets(targets)
     hits = targets.count("O")
     print(f"You hit {hits} of 5 targets")
 
