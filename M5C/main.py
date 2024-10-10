9402dedf6b0c11fae52eb79b44abf7dd052196f8
Zaidoun Khalid <khalidzaidoun@icloud.com>
2024-10-10 23:46:21 +0200

    Refactored, improved typing, and error handling for reading and writing data

--- M5C/main.py
+++ M5C/main.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 import json
 
 
@@ -9,10 +10,13 @@ class User:
         self.data = data
 
     def add_item(self, item: str):
-        self.data["items"].append(item)
-        print(f"'{item}' added to items")
+        if item:
+            self.data["items"].append(item)
+            print(f"'{item}' added to items")
+        else:
+            print("Invalid item")
 
-    def delete_item(self, item):
+    def delete_item(self, item: str):
         if item in self.data["items"]:
             self.data["items"].remove(item)
             print(f"'{item}' removed from items")
@@ -34,44 +38,43 @@ class Lagra:
         self.data = self.get_data()
 
     @staticmethod
-    def get_data() -> dict:
-        with open("data.json", "r") as file:
-            data = json.load(file)
-            return data
+    def get_data(path=Path("data.json")) -> dict:
+        if path.exists():
+            with open(path, "r") as file:
+                data = json.load(file)
+                return data
+        return {}
 
-    def save_data(self):
+    def save_data(self, path=Path("data.json")):
         if self.user:
             self.data[self.user.username]["items"] = self.user.data.get("items")
-        with open("data.json", "w") as file:
+        with open(path, "w") as file:
             json.dump(self.data, file)
 
     @staticmethod
     def get_option(options: list) -> str:
-        while (option := input("Option: ")) not in options: print("\nInvalid input.\n")
+        while (option := input("Option: ").strip()) not in options: print("\nInvalid input\n")
         return option
 
     def manage(self):
         while True:
+            self.user.list_items()
             print("\nActions")
             print("a) Add item")
             print("d) Delete item")
-            print("l) List items")
             print("q) Log out\n")
-
-            option = self.get_option(["a", "d", "l", "q"])
+            option = self.get_option(["a", "d", "q"])
             if option == "a":
-                item = input("\nAdd item: ")
+                item = input("\nAdd item: ").strip()
                 self.user.add_item(item)
             elif option == "d":
-                item = input("\nDelete item: ")
+                item = input("\nDelete item: ").strip()
                 self.user.delete_item(item)
-            elif option == "l":
-                self.user.list_items()
             elif option == "q":
                 self.save_data()
                 break
 
-    def log_in(self) -> bool:
+    def credentials(self) -> bool:
         username = input("\nUsername: ")
         password = input("Password: ")
         data = self.data.get(username)
@@ -83,11 +86,24 @@ class Lagra:
             return True
         return False
 
+    def log_in(self):
+        while not self.credentials():
+            print("\nInvalid username or password")
+            print("r) Retry")
+            print("b) Back")
+            print("q) Quit\n")
+            option = self.get_option(["r", "b", "q"])
+            if option == "b":
+                break
+            elif option == "q":
+                sys.exit()
+
     def register(self):
         while True:
             username = input("\nUsername: ")
             if username not in self.data:
-                self.data[username] = {"password": input("Password: "),"items": []}
+                password = input("Password: ")
+                self.data[username] = {"password": password, "items": []}
                 self.save_data()
                 print(f"\nAccount created for {username}")
                 break
@@ -96,29 +112,16 @@ class Lagra:
     def main(self):
         while True:
             print("\nWelcome to Lagra™\n")
-
             print("l) Log in")
             print("r) Register")
             print("q) Quit\n")
-
             option = self.get_option(["l", "r", "q"])
-            if option == "q":
-                sys.exit()
-            elif option == "r":
+            if option == "r":
                 self.register()
                 continue
-
-            while not self.log_in():
-                print("\nInvalid username or password")
-                print("r) Retry")
-                print("b) Back")
-                print("q) Quit\n")
-
-                option = self.get_option(["r", "b", "q"])
-                if option == "b":
-                    break
-                elif option == "q":
-                    sys.exit()
+            elif option == "q":
+                sys.exit()
+            self.log_in()
 
 
 if __name__ == "__main__":
